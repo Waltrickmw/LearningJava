b2df48e3f9b3c767a2a8dad11e3500f9ecb2e6f0
Waltrickmw <66733246+Waltrickmw@users.noreply.github.com>
2022-01-25 17:20:10 -0400

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,8 @@
+// Primeiro exercício em Java 14 - " Hello World " - beecrowd
+
+import java.util.IOException;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
