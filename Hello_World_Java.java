ef8dfa87809c248a8f17080b6085ada5e21e3088
Waltrickmw <66733246+Waltrickmw@users.noreply.github.com>
2022-01-25 16:55:43 -0400

    Update Hello_World_Java.java

--- Hello_World_Java.java
+++ Hello_World_Java.java
@@ -1,7 +1,5 @@
 // Primeiro exercício em Java 14 - " Hello World " - beecrowd
 
-import java.util.IOException;
-
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
